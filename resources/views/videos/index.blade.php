be0f88dfda7458fa9fb088af14bf746bc7074ca3
Pedro de Robles <piterddr@gmail.com>
2021-12-11 01:26:46 -0300

    Header Youtube. menu + logo + input + perfil. Sin funcionalidad, solo visual

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -6,9 +6,19 @@
 
         <title>Youtube</title>
 
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <header>
-        
+        <div class="bg-gray-800 flex items-center justify-between  w-full h-12">
+            <div class="flex items-center ml-2">
+                <img class="w-6" src="/img/menu-burger.png" alt="">
+                <a href="{{ url('dashboard') }}">
+                    <img class="w-20 ml-4" src='/img/youtube.png' alt="youtube logo">
+                </a>
+            </div>
+            <input class="rounded-md py-0" type="text" value="Search">
+            <img class="rounded-full w-8 mr-2" src="/img/perfil.jpg" alt="Foto de perfil">
+        </div>
     </header>
     <body>
         @forelse ($videos as $video)
