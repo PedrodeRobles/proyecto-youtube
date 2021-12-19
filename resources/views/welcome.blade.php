d9829ebaec14211be3860bf3773e60ed95d1b3a8
Pedro de Robles <piterddr@gmail.com>
2021-12-18 23:43:44 -0300

    Se agrego el nombre del canal, el tiempo created_at. Mejoras de diseño en los videos y en el menu izquierdo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,9 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <header>
-        <div class="bg-gray-900 flex items-center justify-between  w-full h-12">
-            <div class="flex items-center ml-2">
-                <img class="w-6" src="/img/menu-burger.png" alt="">
-                <a href="{{ url('dashboard') }}">
+        <div class="bg-gray-800 flex fixed items-center justify-between  w-full h-12">
+            <div class="flex items-center ml-8">
+                <a href="{{ asset('/') }}">
                     <img class="w-20 ml-4" src='/img/youtube.png' alt="youtube logo">
                 </a>
             </div>
@@ -23,43 +22,46 @@
         </div>
     </header>
     <body class="bg-gray-900">
-        
-
-        {{-- Usuario y boton de subir video--}}
-        <div class="flex justify-around items-center w-full bg-gray-800 mb-8">
-            <div class="flex items-center py-8">
-                <img class="rounded-full w-13 mr-2" src="/img/perfil.jpg" alt="Foto de perfil">
-                {{-- <p class="text-white">{{ $video->user->name }}</p> --}}
+        <div class="flex">
+            {{-- Menu --}}
+            <div class="fixed h-screen w-12 bg-gray-800">
+                <img src="/img/menu-burger.png" alt="Menu burger" class="w-6 m-auto pt-3" >
+                <a href="{{ asset('/') }}">
+                    <img src="/img/home.png" alt="home" class="w-6 m-auto pt-12">
+                </a>
+                <p class="text-white text-xs text-center">Home</p>
             </div>
-            <div>
-                <button class="py-2 px-4 bg-blue-600 hover:bg-blue-500 text-white font-semibold rounded-lg">
-                    <a href="{{ route('videos.create') }}">Subir video</a>
-                </button>
-            </div>
-        </div>
             
-        {{-- Videos subidos --}}
-        <div class="grid grid-cols-4">
-            @forelse ($videos as $video)
-                    <div class="rounded-md border-2 border-gray-800 mx-auto mb-10 hover:bg-gray-800">
-                        <img src="{{ $video->get_image }}" class="w-80 h-44 object-cover">
-                        <div class="text-white font-bold p-2">
-                            {{ $video->title }}
+            {{-- Videos subidos --}}
+            <div class="grid grid-cols-5 gap-2 pl-16 pr-4 pt-16">
+                @forelse ($videos as $video)
+                        <div class="rounded-md mx-auto mb-8 hover:bg-gray-800">
+                            <img src="{{ $video->get_image }}" class="w-80 h-40 object-cover border-2 border-gray-800">
+                            <div class="flex pt-2">
+                                <div class="">
+                                    <img class="rounded-full w-8" src="/img/perfil.jpg" alt="Foto de perfil">
+                                </div>
+                                <div class="pl-2">
+                                    <div class="text-white text-md">
+                                        {{ $video->title }}
+                                    </div>
+                                    <div class="text-gray-400 hover:text-white text-sm">
+                                        <a href="#">
+                                            {{ $video->user->name }}
+                                        </a>
+                                    </div>
+                                    <div class="text-gray-400 text-xs">
+                                        {{ $video->created_at->diffForHumans() }}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="flex justify-around py-4">
-                            <a href="{{ route('videos.edit', $video) }}" class="py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-md">Editar</a>
-                            <form action="{{ route('videos.destroy', $video) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button onclick="return confirm('¿Desea eliminar este video?')" class="py-2 px-4 bg-red-600 hover:bg-red-500 text-white rounded-md">Eliminar</button>
-                            </form>
-                        </div>
+                @empty
+                    <div class="flex justify-center">
+                        <p class="text-white font-bold text-xl">En este momento ningún usuario subio videos</p>
                     </div>
-            @empty
-                <div class="flex justify-center">
-                    <p class="text-white font-bold text-xl">No hay videos subidos</p>
-                </div>
-            @endforelse
+                @endforelse
+            </div>
         </div>
     </body>
 </html>
