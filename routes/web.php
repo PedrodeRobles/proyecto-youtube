eebef8235547faf91ff5cec3e53778e5fa575039
Pedro de Robles <piterddr@gmail.com>
2021-12-20 17:36:39 -0300

    Test para invitados cuando quieran postear, actualizar o borrar un comentario, que se redirecciones hacia 'login'. Test realizaco con exito

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\ComentaryController;
 use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Models\Comentary;
 
 Route::get('/', [PageController::class, 'home']);
 Route::get('video/{video}', [PageController::class, 'show'])->name('video');
+// Probando comentarios
+Route::post('video/{video}', [ComentaryController::class, 'store'])->middleware('auth');
+Route::put('video/{video}', [ComentaryController::class, 'update'])->middleware('auth');
+Route::delete('video/{video}', [ComentaryController::class, 'destroy'])->middleware('auth');
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
